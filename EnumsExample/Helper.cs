96cc076c06bd5f7a9f276f2c1c3a8e52eb9fe033
R M Shahidul Islam <shahedbddev@gmail.com>
2023-11-26 15:51:42 +0600

    + Project Code Added

---
+++ EnumsExample/Helper.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace EnumsExample
+{
+    public static class Helper
+    {
+        [Flags]
+        public enum FileAccessEnum
+        {
+            Read = 1,
+            Write = 2,
+            Execute = 4
+        }
+        public enum Status
+        {
+            [Description("Active Status")]
+            Active,
+            [Description("Inactive Status")]
+            Inactive,
+            [Description("Pending Status")]
+            Pending,
+            [Description("InProgress Status")]
+            InProgress,
+        }
+        public enum WeekDays
+        {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7,
+        }
+        public enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
+
+        public enum TrafficLight
+        {
+            Red,
+            Yellow,
+            Green
+        }
+
+
+        public static bool IsInProcess(this Status status)
+        {
+            return status == Status.Pending || status == Status.InProgress;
+        }
+
+        public static IEnumerable<string> GetDescriptions<T>()
+        {
+            var attributes = typeof(T).GetMembers()
+                .SelectMany(member => member.GetCustomAttributes(typeof(DescriptionAttribute), true).Cast<DescriptionAttribute>())
+                .ToList();
+
+            return attributes.Select(x => x.Description);
+        }
+    }
+}
---
+++ EnumsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using static EnumsExample.Helper;
+
+FileAccessEnum _FileAccessEnum = FileAccessEnum.Read | FileAccessEnum.Write;
+Console.WriteLine(_FileAccessEnum);
+
+var _StatusDes = GetDescriptions<Status>();
+foreach (var item in _StatusDes)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in Enum.GetValues(typeof(WeekDays)))
+{
+    Console.WriteLine($"{item} == {(int)item}");
+}
+
+var orderStatus = Status.Pending;
+Console.WriteLine(orderStatus.IsInProcess());
+
+Console.WriteLine(LogLevel.Error + ": An error occurred");
+
+// Nullable enum
+TrafficLight? currentLight = null;
